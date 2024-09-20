e7970b14429f54cf0bb3b9fe0367faac3bdd1858
Mohith <mohithjegan@gmail.com>
2024-09-20 11:35:12 -0400

    Add project files.

---
+++ ExcerciseTwo/Controllers/InputController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcerciseTwo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputController : ControllerBase
+    {
+        [HttpGet(template:"Lucky/{Number}")]
+        public string Lucky(int Number)
+        {
+            return $"Entered number is {Number}";
+        }
+
+        [HttpGet(template:"Unlucky")]
+        public string Unlucky(int Number)
+        {
+            return $"Entered number is {Number}";
+        }
+    }
+}
